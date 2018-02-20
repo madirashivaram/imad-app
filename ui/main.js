c54270f9fe5d3a46fcd0da9d7cabb533b56e127a
madirashivaram <36044007+madirashivaram@users.noreply.github.com>
2018-02-20 18:06:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,16 @@
 //Counter code
 var button = document.getElementById('counter');
-var coumter = 0;
-
 button.onclick = function () {
-    counter = counter + 1;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
     var span = document.getElementById('count');
     span.innerHTML = counter.tostring();
+            }
+        }
+    };
+    request.open('GET', 'http://sivaramreddy79.imad.hasura-app.io/', true);
+    request.send(null);
 };
