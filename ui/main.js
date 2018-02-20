8c4ca632aec801814f921c6afbdc1ba825c95684
madirashivaram <36044007+madirashivaram@users.noreply.github.com>
2018-02-20 17:52:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
+var coumter = 0;
+
+button.onclick = function () {
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.tostring();
+};
